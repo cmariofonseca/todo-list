b62f713f853711a29bf09a81842d67ef0926205d
Fonseca Naranjo Carlos <Carlos.Fonseca+Suracl@externos.afpcapital.cl>
2025-06-12 00:13:00 -0500

    Primer commit: Se crea repositorio base y se importan las librerias necesarias para las tareas requeridas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireRemoteConfigModule } from '@angular/fire/compat/remote-config';
+import { firebaseConfig } from '../environments/firebase-config';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireRemoteConfigModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDzJ_3U7P9J3NqmMCQlv1Mq3nTIfQB_dPA',
+  authDomain: 'todo-list-cf1d3.firebaseapp.com',
+  projectId: 'todo-list-cf1d3',
+  storageBucket: 'todo-list-cf1d3.firebasestorage.app',
+  messagingSenderId: '233757211844',
+  appId: '1:233757211844:web:a1bb9533c7fe366f62dd91',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
