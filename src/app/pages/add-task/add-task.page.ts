cc356eb0288999dc9c60350205bd8d7fda2bf8f7
Fonseca Naranjo Carlos <Carlos.Fonseca+Suracl@externos.afpcapital.cl>
2025-06-12 01:57:17 -0500

    feat(app): Creacion base del menú lateral funcional y páginas base listas (HomePage y AddTaskPage)

---
+++ src/app/pages/add-task/add-task.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.page.html',
+  styleUrls: ['./add-task.page.scss'],
+  standalone: false,
+})
+export class AddTaskPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
